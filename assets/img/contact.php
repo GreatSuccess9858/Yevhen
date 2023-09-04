b5bbd460cb74eed2772cbc2c3679126bf2cbcbb3
HexHacker <nalpa199858@gmail.com>
2023-09-05 01:04:23 +0900

    account info

---
+++ assets/img/contact.php
@@ -0,0 +1,25 @@
+
+===== Client Contact Info =====
+Usa client: +1 843 499 4853(IOS)
+Laravel10 client: Telegram;
+		   https://t.me/seven_star_99  
+		   Skype : madnazz
+Spain Canvas Client: Telegram;
+			https://t.me/carlus9909
+		     Whatsapp:
+			+34 692 09 70 75
+Aircom: 	     Telegram:
+			https://t.me/Marko2M
+		     linkedin:			
+			https://www.linkedin.com/in/markomarko/
+			https://www.linkedin.com/company/aircom-bg-ltd/
+Next js Client:      Telegram
+			https://t.me/Paget96
+Israel python: https://t.me/Ayoub00972
+
+===== Developer Contact Info =====
+
+Ghana Python Expert:
+		    Telegram : https://t.me/Cipherx20  +254 701 455 288
+Laravel developer:
+		    Telegram : https://t.me/Rohan9202
